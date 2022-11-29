fdf3dc16fe2fe014381e68c254ff9d25b5227f43
akshatjain1004 <jain.53@iitj.ac.in>
2022-11-29 12:06:15 +0530

    adds code for directed graph matrix

--- autoNormalizer.h
+++ autoNormalizer.h
@@ -21,10 +21,17 @@ class AutoNormalizer {
         Fds = _Fds;
     }
     void start() {
-        auto dm= dependancyMatrix();
-        for(int i=0;i< dm.size();i++){
-            for(int j=0;j<dm[0].size();j++){
-                cout<<dm[i][j]<<" ";
+        // auto dm= dependancyMatrix();
+        // for(int i=0;i< dm.size();i++){
+        //     for(int j=0;j<dm[0].size();j++){
+        //         cout<<dm[i][j]<<" ";
+        //     }
+        //     cout<<'\n';
+        // }
+        auto dg= directedGraphMatrix();
+        for(int i=0;i< dg.size();i++){
+            for(int j=0;j<dg[0].size();j++){
+                cout<<dg[i][j]<<" ";
             }
             cout<<'\n';
         }
@@ -48,6 +55,24 @@ class AutoNormalizer {
             num++;
         }
         return dm;
+    }
+    vector<vector<int>> directedGraphMatrix(){
+        auto dm= dependancyMatrix();
+        vector<vector<int>> dg(numFds,vector<int>(numFds,0));
+        for(int i=0;i<numFds;i++){
+            for(auto k: Fds[i].LHS){
+                for(int j=0;j<numFds;j++){
+                    if(dm[j][k]!=0 && dg[j][i]!=-1){
+                        dg[j][i]=1;
+                    }
+                    else{
+                        dg[j][i]=-1;
+                    }
+                    
+                }
+            }
+        }
+        return dg;
     }
 
 };
